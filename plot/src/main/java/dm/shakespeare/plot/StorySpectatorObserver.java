992b070a2c52eccc45b12c20add4445a5b080e70
Davide Maestroni <davide.maestroni@gmail.com>
2019-06-24 16:17:44 +0200

    StorySpectatorObserver + cosmetics

---
+++ plot/src/main/java/dm/shakespeare/plot/StorySpectatorObserver.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 Davide Maestroni
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dm.shakespeare.plot;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by davide-maestroni on 04/10/2019.
+ */
+public interface StorySpectatorObserver<T> {
+
+  void onEffect(@NotNull Spectator spectator, T effect) throws Exception;
+
+  void onEnd(@NotNull Spectator spectator) throws Exception;
+
+  void onIncident(@NotNull Spectator spectator, @NotNull Throwable incident) throws Exception;
+}
